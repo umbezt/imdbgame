2bbceb71b3e7e2f3fd4a17b7e9f958eabb8f11bd
Tapiwa Maumbe <tapiwamaumbe@gmail.com>
2020-11-11 08:26:08 +0200

    Initial Commit added migrations

---
+++ database/migrations/2020_11_11_055030_create_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $this->integer('state')->default(1);
+            $this->unsignedBigInteger('player1')->nullable();
+            $this->unsignedBigInteger('player2')->nullable();
+            $this->unsignedBigInteger('winner')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
